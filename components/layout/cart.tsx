75d28978526da43014996a460695d9320a3d356c
flashey35 <aswinbhusal35@gmail.com>
2024-03-08 11:36:39 +0545

    Login signup UI

--- components/layout/cart.tsx
+++ components/layout/cart.tsx
@@ -2,55 +2,66 @@ import { useShoppingCart } from '@/providers/shoppingCart-provider';
 import React from 'react';
 
 const Cart = () => {
-  const { closeCart, cartItems, increaseCartQuantity, decreaseCartQuantity } =
-    useShoppingCart();
+  const {
+    closeCart,
+    cartItems,
+    cartQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity
+  } = useShoppingCart();
   console.log(cartItems);
   return (
     <div className="fixed inset-y-0 right-0 w-80 bg-white p-4 shadow-lg">
       <div className="flex items-center justify-between">
-        <h1 className=" text-xl font-semibold">Your Cart</h1>
+        <h1 className=" m-3  text-xl font-semibold"> Cart</h1>
         <button className=" text-2xl font-bold" onClick={closeCart}>
           X
         </button>
       </div>
       <div>
-        {cartItems.map((item) => (
-          <div key={item.id}>
-            <div className="mb-4 flex items-center justify-between">
-              <div className="flex items-center">
-                <img
-                  src="/carrot.jpg"
-                  alt={item.title}
-                  className="mr-4 h-16 w-16"
-                />
-                <div>
-                  <p className="text-lg font-semibold">{item.title}</p>
-                  <p className="text-gray-500">Rs {item?.price}</p>
+        {cartQuantity > 0 ? (
+          cartItems.map((item) => (
+            <div key={item.id}>
+              <div className="mb-4 flex items-center justify-between">
+                <div className="flex items-center">
+                  <img
+                    src="/carrot.jpg"
+                    alt={item.title}
+                    className="mr-4 h-16 w-16"
+                  />
+                  <div>
+                    <p className="text-lg font-semibold">{item.title}</p>
+                    <p className="text-gray-500">Rs {item?.price}</p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <button
+                    onClick={() => decreaseCartQuantity(item.id)}
+                    className="rounded-l-md bg-gray-200 px-3 py-1 text-gray-700 hover:bg-gray-300"
+                  >
+                    -
+                  </button>
+                  <span className="bg-gray-100 px-3 py-1">{item.quantity}</span>
+                  <button
+                    onClick={() =>
+                      increaseCartQuantity({
+                        id: item?.id,
+                        quantity: item.quantity
+                      })
+                    }
+                    className="rounded-r-md bg-gray-200 px-3 py-1 text-gray-700 hover:bg-gray-300"
+                  >
+                    +
+                  </button>
                 </div>
               </div>
-              <div className="flex items-center">
-                <button
-                  onClick={() => decreaseCartQuantity(item.id)}
-                  className="rounded-l-md bg-gray-200 px-3 py-1 text-gray-700 hover:bg-gray-300"
-                >
-                  -
-                </button>
-                <span className="bg-gray-100 px-3 py-1">{item.quantity}</span>
-                <button
-                  onClick={() =>
-                    increaseCartQuantity({
-                      id: item?.id,
-                      quantity: item.quantity
-                    })
-                  }
-                  className="rounded-r-md bg-gray-200 px-3 py-1 text-gray-700 hover:bg-gray-300"
-                >
-                  +
-                </button>
-              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <>
+            <h1>Cart is Empty</h1>
+          </>
+        )}
       </div>
     </div>
   );
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -8,22 +8,21 @@ const Navbar = () => {
   return (
     <div className="  ml-4 mr-4 mt-2 flex min-h-[60px] min-w-10 items-center  justify-between gap-6 rounded-[20px] p-4 ">
       <div className="image-logo flex items-center gap-5 text-2xl  font-bold">
-        <h2 className="text-2xl">LOGO</h2>
-        <h1>AgroNepal</h1>
+        <h1 className="font-mono">AgroNepal</h1>
       </div>
       <div className="list flex ">
-        <ol className="flex items-center gap-16 text-2xl font-bold ">
+        <ol className="flex items-center gap-16 font-mono text-2xl ">
           <li className="cursor-pointer ">Home</li>
           <li className="cursor-pointer ">Product</li>
           <li className="cursor-pointer ">Our Sevices</li>
           <li className="cursor-pointer ">About US</li>
         </ol>
       </div>
-      <div className="flex items-center gap-5 text-2xl font-bold ">
-        <h1 className="flex h-9 w-24 cursor-pointer items-center justify-center rounded-lg bg-inherit hover:bg-white ">
+      <div className="flex  items-center gap-2 text-2xl font-bold ">
+        <h1 className="h-9 cursor-pointer items-center justify-center rounded-lg bg-inherit font-mono hover:bg-white ">
           SignUp
         </h1>
-        <h1 className=" flex h-9 w-24 cursor-pointer items-center justify-center  rounded-lg bg-lime-300  hover:border-[1px]">
+        <h1 className=" h-9 cursor-pointer items-center  justify-center rounded-lg bg-lime-300 px-2  font-mono  hover:border-[1px]">
           Login
         </h1>
       </div>
